23e53f12f4a1ccfa110190b28ab102bb8801750c
sonchaegeon <thscorjs1002@naver.com>
2021-07-02 11:46:19 +0900

    ⚡️ :: import typeorm, graphql, question modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { QuestionModule } from './question/question.module';
+import { TypeOrmConfigModule } from './typeorm/typeorm-config.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmConfigModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    QuestionModule,
+  ],
 })
 export class AppModule {}
